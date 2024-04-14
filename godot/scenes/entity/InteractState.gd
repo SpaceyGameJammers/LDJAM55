f159532ac34f2d4439c16644b48c697f171c0274
Savalige <matsbennervall@gmail.com>
2024-04-14 19:02:26 +0200

    Fixed error with entity not working

--- godot/scenes/entity/InteractState.gd
+++ godot/scenes/entity/InteractState.gd
@@ -16,7 +16,7 @@ func enter(_msg := {}):
 	target = _msg["target"]
 	character.position = target
 	if workstation:
-		if workstation.work_done.is_connected(_on_timer_timeout):
+		if !workstation.work_done.is_connected(_on_timer_timeout):
 			workstation.work_done.connect(_on_timer_timeout)
 		if character.occupation == character.OCCUPATION.CUSTOMER:
 			workstation.customer_interact(character)
