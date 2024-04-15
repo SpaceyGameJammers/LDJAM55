652a3b253e02083cfa8851d34d4263b63a6cc25c
Savalige <matsbennervall@gmail.com>
2024-04-15 15:31:28 +0200

    Dont task skip

--- godot/scenes/entity/TargetState.gd
+++ godot/scenes/entity/TargetState.gd
@@ -3,13 +3,19 @@ class_name TargetState
 
 @export var character: Entity
 
+var target
+var type
+var station:Workstation
+var has_state: bool = false
+
 func enter(_msg := {}):
-	var target
-	var type
-	var station:Workstation
-	
 	if !_msg.has("state"):
+		has_state = true
+
+func update(_delta):
+	if has_state:
 		if character.targets.is_empty():
+			print(str(self) + ": NEED JOBS")
 			match character.occupation:
 				character.OCCUPATION.CUSTOMER:
 					character.targets = character.customer.duplicate()
@@ -24,47 +30,44 @@ func enter(_msg := {}):
 	else:
 		character.targets = [character.INTERACTION.LEAVE]
 	
-	while type == null or target == null:
-		print(str(self) + ": " + str(character.targets))
-		if !character.targets.is_empty():
-			match character.targets[0]:
-				character.INTERACTION.ORDER:
-					station = WorkstationManager.occupy_customer_workstation(WorkstationManager.WORKSTATION.REGISTER)
-					type = character.INTERACTION.ORDER
-					if station:
-						target = station.get_customer_position()
-					else:
-						print("No register found")
-				character.INTERACTION.WAIT:
-					station = WorkstationManager.occupy_customer_workstation(WorkstationManager.WORKSTATION.TABLE)
-					type = character.INTERACTION.WAIT
-					if station:
-						target = station.get_customer_position()
-					else:
-						print("No table found")
-				character.INTERACTION.LEAVE:
-					type = character.INTERACTION.LEAVE
-					target = character.leave.global_position
-				character.INTERACTION.COOK_POT:
-					type = character.INTERACTION.COOK_POT
-					station = WorkstationManager.occupy_workstation(WorkstationManager.WORKSTATION.POT)
-					if station:
-						target = station.get_worker_position()
-				character.INTERACTION.REGISTER:
-					type = character.INTERACTION.REGISTER
-					station = WorkstationManager.occupy_workstation(WorkstationManager.WORKSTATION.REGISTER)
-					if station:
-						target = station.get_worker_position()
-				character.INTERACTION.TABLE:
-					type = character.INTERACTION.TABLE
-					station = WorkstationManager.occupy_workstation(WorkstationManager.WORKSTATION.TABLE)
-					if station:
-						target = station.get_worker_position()
-			character.targets.remove_at(0)
-		else:
-			break
+	print(str(self) + ": " + str(character.targets))
+	if !character.targets.is_empty():
+		match character.targets[0]:
+			character.INTERACTION.ORDER:
+				station = WorkstationManager.occupy_customer_workstation(WorkstationManager.WORKSTATION.REGISTER)
+				type = character.INTERACTION.ORDER
+				if station:
+					target = station.get_customer_position()
+				else:
+					print("No register found")
+			character.INTERACTION.WAIT:
+				station = WorkstationManager.occupy_customer_workstation(WorkstationManager.WORKSTATION.TABLE)
+				type = character.INTERACTION.WAIT
+				if station:
+					target = station.get_customer_position()
+				else:
+					print("No table found")
+			character.INTERACTION.LEAVE:
+				type = character.INTERACTION.LEAVE
+				target = character.leave.global_position
+			character.INTERACTION.COOK_POT:
+				type = character.INTERACTION.COOK_POT
+				station = WorkstationManager.occupy_workstation(WorkstationManager.WORKSTATION.POT)
+				if station:
+					target = station.get_worker_position()
+			character.INTERACTION.REGISTER:
+				type = character.INTERACTION.REGISTER
+				station = WorkstationManager.occupy_workstation(WorkstationManager.WORKSTATION.REGISTER)
+				if station:
+					target = station.get_worker_position()
+			character.INTERACTION.TABLE:
+				type = character.INTERACTION.TABLE
+				station = WorkstationManager.occupy_workstation(WorkstationManager.WORKSTATION.TABLE)
+				if station:
+					target = station.get_worker_position()
 		
-	if target:
+	if target != null and type != null:
+		character.targets.remove_at(0)
 		state_machine.transition_to("WalkState", 
 		{ 
 			"type": type, 
